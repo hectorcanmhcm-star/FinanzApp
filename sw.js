24ae28239b2910430cd6ca45086fc7dbfd77b8e4
hectorcanmhcm-star <hectorcanm.hcm@gmail.com>
2026-09-20 19:49:23 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,5 @@
 const CACHE_NAME = 'finanzapp-cache-v2';
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
+co
   'https://cdn.tailwindcss.com',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
 ];
